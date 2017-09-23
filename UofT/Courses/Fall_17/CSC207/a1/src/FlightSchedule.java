ba0cb402dbdaf3a28c72f60fa9cb32805cfc98c1
fernandap <fernanda.palaciosaltamirano@mail.utoronto.ca>
2017-09-23 16:55:20 -0400

    Wrote structure for FlightSchedule

--- UofT/Courses/Fall_17/CSC207/a1/src/FlightSchedule.java
+++ UofT/Courses/Fall_17/CSC207/a1/src/FlightSchedule.java
@@ -1,5 +1,28 @@
 public class FlightSchedule {
+
+    // Do I need a constructor? Don't think so
+
+    public String searchAirport(){
+        // read file called FlightList.txt as input.
+
+        // print to the screen: Enter the name of an airport
+
+        // if the user types the word exit, your program should terminate.
+
+        // check if airport is in file
+
+        // if it is return such airport in the format 'PVG (AC123, WS592, AC225)'
+
+        //  Otherwise, the program will continue to prompt the user for more airports.
+        return null;
+    }
+
+
 	public static void main(String[] args) {
+        FlightSchedule fs = new FlightSchedule();
+
+        // fs.searchAirport(FlightList.txt);
+
 
 	}
 }
