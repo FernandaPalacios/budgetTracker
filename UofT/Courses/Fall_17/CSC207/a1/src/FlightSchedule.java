bd5a01c9d2fca4f32f8edb99836088452f2aa58a
fernandap <fernanda.palaciosaltamirano@mail.utoronto.ca>
2017-09-24 18:54:57 -0400

    Createdsome  helper functions

--- UofT/Courses/Fall_17/CSC207/a1/src/FlightSchedule.java
+++ UofT/Courses/Fall_17/CSC207/a1/src/FlightSchedule.java
@@ -10,10 +10,10 @@ public class FlightSchedule {
 
 
         // prompt user
-
         String userAirport = promptAirport();
 
         // Validate input
+
         // Case 1: exit
         if(userAirport.equals("exit")){
             // terminate program
@@ -23,7 +23,8 @@ public class FlightSchedule {
         // Case 2: Invalid airport
         else if(userAirport.length() != 3){
             // prompt user again
-            promptAirport();
+            System.out.println("This is not a valid airport");
+            searchAirport(fileName);
 
         }
 
@@ -32,9 +33,8 @@ public class FlightSchedule {
         ArrayList<Airport> seenAirports = readAirports(fileName);
 
 
-
         // Search for match
-        boolean match = hasMatch(userAirport, seenAirports);
+        // boolean match = hasMatch(userAirport, seenAirports);
 
 
 
@@ -52,18 +52,88 @@ public class FlightSchedule {
     }
 
     public ArrayList<Airport> readAirports(String fileName){
+
+        // Accumulate Airports
+        ArrayList<Airport> answer = new ArrayList<Airport>();
+
+        // start reading from file
+        File file = new File(fileName);
+        Scanner scannerFile = null;
+        try {
+            scannerFile = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (scannerFile.hasNextLine()) {
+            String currLine = scannerFile.nextLine();
+
+            // split string by '|'
+            String[] parts = currLine.split("\\|");
+
+            // construct Flight
+            String flightName = parts[0].split("\\s")[0];
+            String flightDate = parts[0].split("\\s")[1];
+
+            Flight currFlight = new Flight(flightName, flightDate);
+
+            // Extract airports to be added to this Flight
+            String[] airportNamesArr = Arrays.copyOfRange(parts, 1, parts.length);
+            ArrayList<String> airportNames = new ArrayList<String>(Arrays.asList(airportNamesArr));
+
+
+            // iterate over airports
+            for(String airportName : airportNames){
+                // check if we've seen this airportName
+                if(!hasMatch(airportName, answer)){
+                    System.out.println(airportName);
+                }
+
+            }
+
+
+        }
+
         return null;
     }
 
     public boolean hasMatch(String AirportName, ArrayList<Airport> Airports){
-        return false;
+        // accumulate names
+        ArrayList<String> names = new ArrayList<String>();
+
+        // iterate over airports to extract names
+        for(Airport a: Airports){
+            String currName = a.getName();
+            if(!names.contains(currName)){
+                names.add(currName);
+            }
+        }
+
+        // return whether AirportName is in names
+        return names.contains(AirportName);
     }
+
 
 
 	public static void main(String[] args) {
         FlightSchedule fs = new FlightSchedule();
-
         fs.searchAirport("FlightList.txt");
 
-	}
+        // Airport a1 = new Airport("YYZ");
+        // Airport a2 = new Airport("YVR");
+        // Airport a3 = new Airport("YHZ");
+        // Airport a4 = new Airport("YUL");
+        // Flight f1 = new Flight("AC123", "12-09-2017");
+        //
+        // ArrayList<Airport> airportsTester = new ArrayList<Airport>();
+        // airportsTester.add(a1);
+        // airportsTester.add(a2);
+        // airportsTester.add(a3);
+        // airportsTester.add(a4);
+        //
+        //
+        // System.out.println(fs.hasMatch( "YYZ", new ArrayList<Airport>())); // False
+        // System.out.println(fs.hasMatch( "GRU", airportsTester)); // False
+
+    }
 }
