c240f4729be2050e70d86ca8723ed590134dce00
fernandap <fernanda.palaciosaltamirano@mail.utoronto.ca>
2017-09-24 16:13:46 -0400

    Broke down structure

--- UofT/Courses/Fall_17/CSC207/a1/src/FlightSchedule.java
+++ UofT/Courses/Fall_17/CSC207/a1/src/FlightSchedule.java
@@ -8,74 +8,55 @@ public class FlightSchedule {
 
     public String searchAirport(String fileName){
 
-        // print to the screen: Enter the name of an airport
-        System.out.println("Enter the name of an airport ");
 
-        Scanner scannerPrompt = new Scanner(System.in);
-        String  searchedAirport  = scannerPrompt.nextLine();
+        // prompt user
 
-        // exit
-        if(searchedAirport.equals("exit")){
-           // terminate program
+        String userAirport = promptAirport();
+
+        // Validate input
+        // Case 1: exit
+        if(userAirport.equals("exit")){
+            // terminate program
             return null;
         }
 
-        // Invalid airport
-        else if(searchedAirport.length() != 3){
+        // Case 2: Invalid airport
+        else if(userAirport.length() != 3){
             // prompt user again
+            promptAirport();
 
         }
 
-        // Valid Airport, check if airport is in file
+        // Case 3: Valid Input
 
-        File file = new File(fileName);
-        Scanner scannerFile = null;
-        try {
-            scannerFile = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        // keep track of airports
+        ArrayList<Airport> seenAirports = readAirports(fileName);
 
 
-        // accumulate airports
 
-        while (scannerFile.hasNextLine()) {
-            String currLine = scannerFile.nextLine();
+        // Search for match
+        boolean match = hasMatch(userAirport, seenAirports);
 
-            // split currLine by '|'
-            String[] parts = currLine.split("\\|");
-
-            // construct Flight
-            String flightName = parts[0].split("\\s")[0];
-            String flightDate = parts[0].split("\\s")[1];
-
-            Flight currFlight = new Flight(flightName, flightDate);
-
-            // Extract airports to be added to this Flight
-            String[] airportsArr = Arrays.copyOfRange(parts, 1, parts.length);
-            ArrayList<String> airports = new ArrayList<String>(Arrays.asList(airportsArr));
-
-
-            // iterate over airports Strings
-
-            // construct airport
-
-            // check if airport was visited by currFlight
-
-            // add this flight to airport
-        }
-
-
-            // look for searchedAirport in airports
-
-            // if found, return such airport in the format 'PVG (AC123, WS592, AC225)'
-
-             //  Otherwise,  prompt the user for more airports.
 
 
         return null;
+
+    }
+
+    public String promptAirport(){
+        // print to the screen: Enter the name of an airport
+        System.out.println("Enter the name of an airport ");
+        Scanner scannerPrompt = new Scanner(System.in);
+        String  answer  = scannerPrompt.nextLine();
+
+        return answer;
+    }
+
+    public ArrayList<Airport> readAirports(String fileName){
+        return null;
+    }
+
+    public boolean hasMatch(String AirportName, ArrayList<Airport> Airports){
+        return false;
     }
 
 
@@ -83,7 +64,6 @@ public class FlightSchedule {
         FlightSchedule fs = new FlightSchedule();
 
         fs.searchAirport("FlightList.txt");
-
 
 	}
 }
